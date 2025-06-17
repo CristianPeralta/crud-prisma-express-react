1957c0b3b72053e7cf41f13a850f6ed5d935feab
Cristian Peralta <cbperaltasegura@gmail.com>
2025-06-17 10:12:37 -0500

    feat: Add public folder

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import ProductRoute from './routes/ProductRoute.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 dotenv.config()
 
 const app = express()
 const port = process.env.APP_PORT || 5000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(ProductRoute)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
