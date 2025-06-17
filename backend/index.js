690a97cd337c24265d9d092db97089a160b68771
Cristian Peralta <cbperaltasegura@gmail.com>
2025-06-17 10:36:31 -0500

    chore: Update port

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 dotenv.config()
 
 const app = express()
-const port = process.env.APP_PORT || 5000
+const port = process.env.PORT || 5000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
